a2eded7e3d8b986db9ee7a132f4487c85bcf665d
Martin Beníček <63211618+MartinBenicek@users.noreply.github.com>
2023-10-17 15:00:37 +0200

    progress

--- krypto/ukol2/script.js
+++ krypto/ukol2/script.js
@@ -1,4 +1,5 @@
-/* z Q se stane  O */
+/* z Q se stane  O EN*/
+/* z J se stane  O CZ*/
 const alphabet = [];
 const czech_alphabet = {
     "Á" : "A",
@@ -15,7 +16,9 @@ const czech_alphabet = {
     "Ú" : "U",
     "Ů" : "U",
     "Ž" : "Z",
-    "Ý" : "Y"
+    "Ý" : "Y",
+    " " : "QXW",
+    "Q" : "O"
 };
 
 for (let i = 0; i < 26; i++) {
@@ -24,6 +27,26 @@ for (let i = 0; i < 26; i++) {
 }
 
 alphabet.splice(alphabet.indexOf("Q"), 1);
+
+
+function filtration(text){
+    let filteredText = text.split("");
+    for(let i = 0; i < filteredText.length; i++) {
+        let character = filteredText[i];
+        if (filteredText[i] === "A" || filteredText[i] === "Z" || filteredText[i] === " ") {
+            continue
+        } else if(alphabet.includes(filteredText[i]) === true){
+            continue;
+        } else if(czech_alphabet[character]){
+            filteredText[i] = czech_alphabet[character];
+        } else{
+            filteredText[i] = " ";
+        }
+    }
+    filteredText = filteredText.join("").replace(/ /g,'');
+    return filteredText;
+}
+
 
 function encryptionKey(text){
     let text_array = text.split("");
@@ -37,28 +60,38 @@ function encryptionKey(text){
     for(let i = 0; i < newArray.length; i++){
         outputSquares[i].value = newArray[i];
     }
-    for(let i = newArray.length - 1; i < alphabet.length; i++){
-        if (newArray.includes(alphabet[i]) === false){
-            outputSquares[i].value = newArray[i];
-        }
-        else{
-            i--
+    let capture = 0;
+    for(let i = 0; i < alphabet.length; i++){
+        if(newArray.includes(alphabet[i]) === false){
+            outputSquares[i - capture + newArray.length].value = alphabet[i];
+        } else{
+            capture++;
         }
     }
 }
+
 
 function pairLetters(text){
     let text_array = text.split("");
     let newArray = []
     let newArray2 = []
+    console.log("asd" + text_array.length);
     for(let i = 0; i < text_array.length; i++){
         newArray.push(text_array[i])
-        if(text_array[i] === text_array[i+1]){
+        if(text_array[i] === text_array[i+1] && text_array[i] != "X"){
             newArray.push("X");
         }
-        if((i + 1) % 2 === 1 && text_array[i+1] == null){
+        if (text_array[i] === text_array[i+1] && text_array[i] === "X"){
+            newArray.push("W"); 
+        }
+        if((i + 1) % 2 === 1 && text_array[i+1] == null && text_array[i] != "X"){
             newArray.push("X"); 
         }
+        if((i + 1) % 2 === 1 && text_array[i+1] == null && text_array[i] === "X"){
+            newArray.push("W"); 
+        }
+        
+        console.log(newArray);
     }
     for(let i = 0; i < newArray.length; i++){
         newArray2.push(newArray[i])
@@ -66,13 +99,22 @@ function pairLetters(text){
             newArray2.push(" ");
         }
     }
-    console.log(newArray);
-    console.log(newArray2);
 }
 
 function encrypt(){
     const textToEncrypt = document.getElementById("text-to-encrypt").value.toUpperCase();
     const textAsKey = document.getElementById("encryption-key").value.toUpperCase();
-    encryptionKey(textAsKey);
+    let filteredKey = filtration(textAsKey)
+    encryptionKey(filteredKey);
     let pairedLetters = pairLetters(textToEncrypt)
 }
+
+
+/*
+vyplnění tabulky - nechat jak je z textu (done)
+ve filtraci Q -> O? - ano
+mezery a čísla taky? - ano
+dešifrování uppercase čísla a mezery (jak u afinní šifry)? - je to tak
+filtrovaný text? - ano
+čísla do textového klíče? - na nás (ne)
+*/
