993b747c906b1165aa9e40460f54ef37b697ebde
MartinBenicek <63211618+MartinBenicek@users.noreply.github.com>
2023-10-30 21:15:06 +0100

    progress 3

--- krypto/ukol3/script.js
+++ krypto/ukol3/script.js
@@ -3,14 +3,19 @@ přepíání tabulky nebo 2 vedle sebe?
 co zaměňovat? podle verze EN/CZ? (J -> I nebo W -> V.)
 tabulka se chová jak u playfairu?
 vytvořit další tabulku pod kde bude klíč se zašifrovnými dvojicemi?
-ddát klíč a ruční matici i pro dešifrování?
+dát klíč a ruční matici i pro dešifrování?
 vlastní matice bude slovo a pak random?
 výpis aktuální šifrovací tabulky - co znamená?
+5x5 MUSÍ být bez čísel?
+tip na/jak udělat mezeru?
+heslo v ručně zadané matici minimální počat znaků?
 
+přepínač pro volbu zadání matice a náhodného generování.  (co bude lepší na vzhledu)
+pro zadání šifrovací matice ručně (spolu s vyřazováním nebo znázorněních zbývajících znaků pro vyplnění),
+
+zobrazit text po filtraci?
 
 pole pro zadání textu pro dešifrování
-pro zadání šifrovací matice ručně (spolu s vyřazováním neboznázorněních zbývajících znaků pro vyplnění),
-přepínač pro volbu zadání matice a náhodného generování.
 výpis zašifrovaného textu (mezeru dlesloupců),
 výpis aktuální šifrovací tabulky,
 tlačítko pro volbu šifrování dešifrování v rámci stejného GUI,
@@ -45,11 +50,80 @@ for (let i = 0; i < 26; i++) {
 }
 alphabet.splice(alphabet.indexOf("J"), 1);
 
-function matrixCheckboxclick(){
-    let matrixCheckbox = document.getElementsByClassName("matrix-checkbox");
-    if(matrixCheckbox[0].checked === true){
-        matrixCheckbox[1].checked = false;
-    } else{
-        matrixCheckbox[1].checked = true;
+const matrixCheckbox = document.getElementsByClassName("matrix-checkbox");
+const matrixCheckboxChoice = document.getElementById("matrix-key");
+
+matrixCheckbox[0].addEventListener("click", () =>{
+    matrixCheckbox[1].checked = false;
+    matrixCheckbox[0].checked = true;
+    matrixCheckboxChoice.disabled = true;
+})
+matrixCheckbox[1].addEventListener("click", () =>{
+    matrixCheckbox[0].checked = false;
+    matrixCheckbox[1].checked = true;
+    matrixCheckboxChoice.disabled = false;
+})
+
+function getMatrixChoice(){
+    let getNumber;
+    for (let i = 0; i < matrixCheckbox.length; i++){
+        if (matrixCheckbox[i].checked === true){
+            getNumber = i;    
+        }
     }
+    return getNumber;
+}
+
+function shuffleAlphabet(array) {
+    array.sort(() => Math.random() - 0.5);
+    return array;
+  }
+
+function fillMatrix(){
+    const outputSquares = document.getElementsByClassName("output-square");
+    let shuffledAlphabet = shuffleAlphabet(alphabet);
+    for(let i = 0; i < alphabet.length; i++){
+        outputSquares[i].value = shuffledAlphabet[i];
+    }
+    let matrix = listToMatrix(outputSquares, 5)
+
+    return matrix;
+}
+
+function findIndexInMatrix(letter, matrix){
+    let position = [];
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix.length; j++){
+            if(letter === matrix[i][j]){
+                position = [i, j];
+            }
+        }
+    }
+    return position;
+}
+
+function listToMatrix(list, elementsPerSubArray) {
+    let matrix = [], i, k;
+
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % elementsPerSubArray === 0) {
+            k++;
+            matrix[k] = [];
+        }
+
+        matrix[k].push(list[i].value);
+    }
+
+    return matrix;
+}
+
+function encrypt(){
+    const textToEncrypt = document.getElementById("text-to-encrypt").value.toUpperCase();
+    const textAsKey = document.getElementById("encryption-key").value.toUpperCase();
+    const printEncryptedText = document.getElementById("encrypted-text");
+    const matrixChoice = getMatrixChoice();
+    if(matrixChoice === 1){
+        console.log("hi");
+    }
+    let matrix = fillMatrix();
 }
